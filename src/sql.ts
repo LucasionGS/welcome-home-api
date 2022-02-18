0465bf83e98ee12ecccac8865230c6b11cccefc3
Lucasion-IonServer <lucasion@hotmail.com>
2022-02-18 18:46:43 +0100

    Removed bloating commented out code

--- src/sql.ts
+++ src/sql.ts
@@ -11,35 +11,6 @@ interface ISQLConfig extends Options {
 }
 
 const configPath = resolve(__dirname + "/../config.json");
-if (!fs.existsSync(configPath)) {
-  // fs.writeFileSync(configPath, JSON.stringify({
-  //   "host": "localhost",
-  //   "username": "root",
-  //   "password": "",
-  //   "database": "welcome-home"
-  // }, null , 2));
-
-  // const lines = [
-  //   "",
-  //   "",
-  //   "",
-  //   `No config file found.`,
-  //   `Created one at ${configPath}`,
-  //   `Please fill in the config file with your MySQL credentials and run the command again.`,
-  //   "",
-  //   `Remember that the database name you choose to use MUST EXIST before running the server.`,
-  //   "",
-  // ];
-  // let spaces: number;
-  // for (let i = 0; i < lines.length; i++) {
-  //   spaces = Math.floor((process.stdout.columns - lines[i].length) / 2);
-  //   console.log(`${" ".repeat(spaces)}${lines[i]}${" ".repeat(spaces)}`);
-  // }
-  // console.log();
-  // process.exit(1);
-
-  
-}
 
 export const sqlConfig: ISQLConfig = {} as any;
 try {
